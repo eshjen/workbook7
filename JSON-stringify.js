0cdece583f60b353f5be0fc6270ae8ccbe17d1d2
Eshei Jenkins <eshjenk@gmail.com>
2024-11-19 14:15:41 -0500

    json demos

---
+++ JSON-stringify.js
@@ -0,0 +1,14 @@
+"use strict";
+
+let person = {
+  first: "Ed",
+  last: "Sheeran",
+};
+// the data type of person is an object
+
+let peeps = [
+  { first: "Ed", last: "Sheeran" },
+  { first: "George", last: "Michael" },
+];
+// the data type of peeps is an array. array of objects
+
---
+++ demos/fetch-async-await.js
@@ -0,0 +1,9 @@
+"use strict";
+
+async function getData() {
+  let promise = fetch("https://jsonplaceholder.typicode.com/users");
+  let response = await promise;
+  let data = await response.json()
+  console.log(data);
+}
+getData();
