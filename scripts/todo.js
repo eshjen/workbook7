f209d901d4aff02a7f21482ee380903097288039
Eshei Jenkins <eshjenk@gmail.com>
2024-11-25 15:35:32 -0500

    todo exercise

--- scripts/todo.js
+++ scripts/todo.js
@@ -1,4 +1,24 @@
 "use strict";
-function showDetails() {
-    console.log("I was here")
+let idInput = document.querySelector("#idInput");
+let detailsDiv = document.querySelector("#detailsDiv")
+
+
+async function getToDo(id) {
+  let promise = fetch("https://jsonplaceholder.typicode.com/todos/" + id);
+  let response = await promise;
+  let todo = await response.json();
+    detailsDiv.innerText = todo.title
+  console.log(todo);
 }
+ 
+function showDetails() {
+  console.log(idInput.value);
+
+  let id = idInput.value;
+  getToDo(id);
+}
+
+
+// let id = 1
+// let message = "Hello, world!" + id
+// console.log(message)
